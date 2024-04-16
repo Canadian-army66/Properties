e0fa21f02ace9dd6164d0bfae25a9fcbd791be72
Canadian-army66 <142023388+Canadian-army66@users.noreply.github.com>
2024-04-16 08:53:53 -0500

    code

---
+++ Properties_Kohnen_Christian/Car.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Properties_Kohnen_Christian
+{
+    class Car //creates the class of Car
+    {
+        private string model; //makes a private class
+        public string Model { get { return model; } set { model = value; } } //makes a protected public Model class
+
+        public string Make { get; set; } //makes a protected public Make class
+    }
+}
---
+++ Properties_Kohnen_Christian/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Properties_Kohnen_Christian
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Car car = new Car(); //assigns a sub class the name of car
+            car.Make = "Mitsubishi"; //assigns the value as Mitsubishi
+            car.Model = "Montero"; //assigns the value as Montero
+
+            Console.WriteLine($"The make of the car is {car.Make} and the model of the car is {car.Model}"); //prints the two classes
+
+            Car carnew = new Car(); //assigns a sub class the name of carnew
+            car.Make = "Jeep"; //assigns the value as Jeep
+            car.Model = "Cherokee"; //assigns the value as Cherokee
+
+            Console.WriteLine($"The make of the car is {car.Make} and the model of the car is {car.Model}"); //prints the two classes
+
+            Car car1 = new Car(); //assigns a sub class the name of car
+            Console.WriteLine($"The make of the car is {car1.Make} and the model of the car is {car1.Model}"); //prints the class but leaves the two values of the maker and model empty
+        }
+    }
+}
