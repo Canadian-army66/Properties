15c1ef870882242e2c5e01c25868c38ae64a8962
Canadian-army66 <142023388+Canadian-army66@users.noreply.github.com>
2024-04-30 08:40:26 -0500

    post-production fixes

--- Properties_Kohnen_Christian/Program.cs
+++ Properties_Kohnen_Christian/Program.cs
@@ -11,10 +11,10 @@
             Console.WriteLine($"The make of the car is {car.Make} and the model of the car is {car.Model}"); //prints the two classes
 
             Car carnew = new Car(); //assigns a sub class the name of carnew
-            car.Make = "Jeep"; //assigns the value as Jeep
-            car.Model = "Cherokee"; //assigns the value as Cherokee
+            carnew.Make = "Jeep"; //assigns the value as Jeep
+            carnew.Model = "Cherokee"; //assigns the value as Cherokee
 
-            Console.WriteLine($"The make of the car is {car.Make} and the model of the car is {car.Model}"); //prints the two classes
+            Console.WriteLine($"The make of the car is {carnew.Make} and the model of the car is {carnew.Model}"); //prints the two classes
 
             Car car1 = new Car(); //assigns a sub class the name of car
             Console.WriteLine($"The make of the car is {car1.Make} and the model of the car is {car1.Model}"); //prints the class but leaves the two values of the maker and model empty
